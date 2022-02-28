6ed2dbbbcc4b1e3f32e33f9dd0669858c2f041c9
zaidkhan5599 <72002930+zaidkhan5599@users.noreply.github.com>
2022-02-28 14:04:06 +0530

    Core Java Code

---
+++ Main.java
@@ -0,0 +1,9 @@
+package com.abstraction;
+
+public class Main {
+
+	public static void main(String[] args) {
+//		P1 ob=new P1();
+	}
+
+}
---
+++ P1.java
@@ -0,0 +1,9 @@
+package com.abstraction;
+
+abstract class P1 {
+	abstract void test();
+	void test1() {
+		System.out.println("From test() of P1 ");
+	}
+	
+}
